2f64cc7ef57c576f6c00d4b527a331833e6dccc7
root <root@doug.lxd>
2026-04-08 17:16:36 +0000

    update setup_manual.sh

--- setup_manual.sh
+++ setup_manual.sh
@@ -6,14 +6,20 @@
 
 echo "Setting up LLM4QPR environment manually..."
 
-echo "Installing PyTorch (CUDA 12.6 wheels) and Python packages..."
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.12 python3.12-venv python3.12-dev
+python3.12 -m venv ~/venvs/py312
+source ~/venvs/py312/bin/activate
+
+echo "Installing PyTorch (CUDA 12.8 wheels) and Python packages..."
 
 # Upgrade pip
 python -m pip install --upgrade pip
 
-# PyTorch + CUDA 12.6 wheels
+# PyTorch + CUDA 12.8 wheels
 python -m pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 \
-  --index-url https://download.pytorch.org/whl/cu126
+  --index-url https://download.pytorch.org/whl/cu128
 
 # Core Python packages
 python -m pip install huggingface_hub wheel alive_progress seaborn
@@ -22,6 +28,8 @@ python -m pip install bitsandbytes==0.46.0
 python -m pip install pandas
 python -m pip install scikit-learn einops numpy pyparsing
 python -m pip install peft==0.15.2
+python -m pip install tiktoken protobuf
+python -m pip install sentencepiece
 
 # FlashAttention prebuilt wheel
 python -m pip install \
